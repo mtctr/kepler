ccf571114f195ef09c3f1146e3b7cf48823e23f4
Mateus Targino <mateustargino95@gmail.com>
2018-12-23 18:40:27 -0300

    plots of moving averages and volatility of flux data/ add dataset of star with confirmed exoplanet

---
+++ datasets/time-curves/confirmed/download_exoarch_22290.bat
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# If wget is not installed on your system,
+# please refer to http://irsa.ipac.caltech.edu/docs/batch_download_help.html.
+#
+# Windows users: the name of wget may have version number (ie: wget-1.10.2.exe)
+# Please rename it to wget in order to successfully run this script
+# Also the location of wget executable may need to be added to the PATH environment.
+#
+wget -O kplr008462852-2009131105131_llc.fits http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/140/24/kplr008462852-2009131105131_llc.fits -a search_66839576.log
+wget -O kplr008462852-2009131105131_llc_lc.tbl http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/145/49/kplr008462852-2009131105131_llc_lc.tbl -a search_66839576.log
+wget -O kplr008462852-2009166043257_llc.fits http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/156/43/kplr008462852-2009166043257_llc.fits -a search_66839576.log
+wget -O kplr008462852-2009166043257_llc_lc.tbl http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/172/05/kplr008462852-2009166043257_llc_lc.tbl -a search_66839576.log
+wget -O kplr008462852-2009259160929_llc.fits http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/221/52/kplr008462852-2009259160929_llc.fits -a search_66839576.log
+wget -O kplr008462852-2009259160929_llc_lc.tbl http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/238/16/kplr008462852-2009259160929_llc_lc.tbl -a search_66839576.log
+wget -O kplr008462852-2009350155506_llc.fits http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/254/72/kplr008462852-2009350155506_llc.fits -a search_66839576.log
+wget -O kplr008462852-2009350155506_llc_lc.tbl http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/271/28/kplr008462852-2009350155506_llc_lc.tbl -a search_66839576.log
+wget -O kplr008462852-2010078095331_llc.fits http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/354/39/kplr008462852-2010078095331_llc.fits -a search_66839576.log
+wget -O kplr008462852-2010078095331_llc_lc.tbl http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/371/38/kplr008462852-2010078095331_llc_lc.tbl -a search_66839576.log
+wget -O kplr008462852-2010174085026_llc.fits http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/188/11/kplr008462852-2010174085026_llc.fits -a search_66839576.log
+wget -O kplr008462852-2010174085026_llc_lc.tbl http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/204/75/kplr008462852-2010174085026_llc_lc.tbl -a search_66839576.log
+wget -O kplr008462852-2010265121752_llc.fits http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/387/55/kplr008462852-2010265121752_llc.fits -a search_66839576.log
+wget -O kplr008462852-2010265121752_llc_lc.tbl http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/404/08/kplr008462852-2010265121752_llc_lc.tbl -a search_66839576.log
+wget -O kplr008462852-2010355172524_llc.fits http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/288/48/kplr008462852-2010355172524_llc.fits -a search_66839576.log
+wget -O kplr008462852-2010355172524_llc_lc.tbl http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/304/82/kplr008462852-2010355172524_llc_lc.tbl -a search_66839576.log
+wget -O kplr008462852-2011073133259_llc.fits http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/421/63/kplr008462852-2011073133259_llc.fits -a search_66839576.log
+wget -O kplr008462852-2011073133259_llc_lc.tbl http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/438/41/kplr008462852-2011073133259_llc_lc.tbl -a search_66839576.log
+wget -O kplr008462852-2011177032512_llc.fits http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/320/51/kplr008462852-2011177032512_llc.fits -a search_66839576.log
+wget -O kplr008462852-2011177032512_llc_lc.tbl http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/337/25/kplr008462852-2011177032512_llc_lc.tbl -a search_66839576.log
+wget -O kplr008462852-2011271113734_llc.fits http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/454/24/kplr008462852-2011271113734_llc.fits -a search_66839576.log
+wget -O kplr008462852-2011271113734_llc_lc.tbl http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/470/94/kplr008462852-2011271113734_llc_lc.tbl -a search_66839576.log
+wget -O kplr008462852-2012004120508_llc.fits http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/488/69/kplr008462852-2012004120508_llc.fits -a search_66839576.log
+wget -O kplr008462852-2012004120508_llc_lc.tbl http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/505/10/kplr008462852-2012004120508_llc_lc.tbl -a search_66839576.log
+wget -O kplr008462852-2012088054726_llc.fits http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/521/37/kplr008462852-2012088054726_llc.fits -a search_66839576.log
+wget -O kplr008462852-2012088054726_llc_lc.tbl http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/538/06/kplr008462852-2012088054726_llc_lc.tbl -a search_66839576.log
+wget -O kplr008462852-2012179063303_llc.fits http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/554/17/kplr008462852-2012179063303_llc.fits -a search_66839576.log
+wget -O kplr008462852-2012179063303_llc_lc.tbl http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/570/84/kplr008462852-2012179063303_llc_lc.tbl -a search_66839576.log
+wget -O kplr008462852-2012277125453_llc.fits http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/587/22/kplr008462852-2012277125453_llc.fits -a search_66839576.log
+wget -O kplr008462852-2012277125453_llc_lc.tbl http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/603/92/kplr008462852-2012277125453_llc_lc.tbl -a search_66839576.log
+wget -O kplr008462852-2013011073258_llc.fits http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/621/70/kplr008462852-2013011073258_llc.fits -a search_66839576.log
+wget -O kplr008462852-2013011073258_llc_lc.tbl http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/638/17/kplr008462852-2013011073258_llc_lc.tbl -a search_66839576.log
+wget -O kplr008462852-2013098041711_llc.fits http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/654/45/kplr008462852-2013098041711_llc.fits -a search_66839576.log
+wget -O kplr008462852-2013098041711_llc_lc.tbl http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/671/21/kplr008462852-2013098041711_llc_lc.tbl -a search_66839576.log
+wget -O kplr008462852-2013131215648_llc.fits http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/687/38/kplr008462852-2013131215648_llc.fits -a search_66839576.log
+wget -O kplr008462852-2013131215648_llc_lc.tbl http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/704/14/kplr008462852-2013131215648_llc_lc.tbl -a search_66839576.log

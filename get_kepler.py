fcad8b2628914bab472565db48d13f40027f530c
Mateus Targino <mateustargino95@gmail.com>
2019-08-11 22:15:43 -0300

    updates

--- get_kepler.py
+++ get_kepler.py
@@ -1,22 +1,31 @@
 import utils
+import time
+
 
 def read_csv(kic):
     periods = []
     df_list = []
-    filenames = utils.get_filenames(utils.BASE_PATH+str(kic),'csv')
+    filenames = utils.get_filenames(utils.BASE_PATH + str(kic), "csv")
+    if len(filenames) <= 5:
+        return {"df_list": df_list, "period": 0.0}
     for idx, filename in enumerate(filenames):
-        data = utils.pd.read_csv(utils.BASE_PATH+str(kic)+'/'+filename)
-        try:
-            res = utils.get_signal_parameters(data.dropna().TIME, data.dropna().PDC_RAW_MEDIAN)
-            periods.append(res["period"])
-        except Exception as e:
-            print(e)
-            print(idx)
+        if idx > 2:
+            data = utils.pd.read_csv(utils.BASE_PATH + str(kic) + "/" + filename)
+            try:
+                res = utils.get_signal_parameters(
+                    data.dropna().TIME, data.dropna().PDC_NORM_FILT
+                )
+                periods.append(res["period"])
+            except Exception as e:
+                print(e)
+                print(idx)
+                print(kic)
 
-        df_list.append(data)
+            df_list.append(data)
 
     period = utils.get_period(periods)
     return {"df_list": df_list, "period": period}
+
 
 def get(kic):
     try:
@@ -28,20 +37,24 @@ def get(kic):
         print(e)
         return e
 
+
 def get_all():
-    return utils.pd.read_csv("datasets/keplerstellar.csv",low_memory=False)
+    return utils.pd.read_csv("datasets/keplerstellar.csv", low_memory=False)
+
 
 def get_kois():
     return utils.pd.read_csv("datasets/kepler-objects-of-interest.csv")
 
+
 def get_binaries():
     return utils.pd.read_csv("datasets/kepler-eclipsing-binary-catalog.csv")
+
 
 def get_all_without_transit():
     kepids = list(get_all().kepid)
     kepids_koi = list(get_kois())
     kepids_binary_stars = list(get_binaries().KIC)
-    
+
     kepids = [x for x in kepids if x not in kepids_koi]
     kepids = [x for x in kepids if x not in kepids_binary_stars]
     return kepids
--- utils.py
+++ utils.py
@@ -11,47 +11,60 @@ from astropy.io import ascii
 from scipy import optimize
 from collections import Counter
 from itertools import groupby
-from statistics import mode, median, mean,stdev
+from statistics import mode, median, mean, stdev
 from tqdm import tqdm
 
-''' Constants with the column names '''
-col = ['SAP_FLUX','PDCSAP_FLUX']
-ecol = ['SAP_FLUX_ERR','PDCSAP_FLUX_ERR']
-col2 = ['F','FPDC']   # Names for the modified columns.
-ecol2 = ['EF','EFPDC']
-''' Constants for paths'''
+""" Constants with the column names """
+col = ["SAP_FLUX", "PDCSAP_FLUX"]
+ecol = ["SAP_FLUX_ERR", "PDCSAP_FLUX_ERR"]
+col2 = ["F", "FPDC"]  # Names for the modified columns.
+ecol2 = ["EF", "EFPDC"]
+""" Constants for paths"""
 BASE_URL = "https://exoplanetarchive.ipac.caltech.edu"
 BASE_PATH = "datasets/light-curves/"
 
 sns.set()
+
+
 def process_data(folder_path):
     """
     Read and process all the kepler data inside a folder based on the path provided
 
     folder_path: path of the folder containing the data filenames
     """
-    filenames_tbl = get_filenames(folder_path,'tbl')
-    filenames_fits = get_filenames(folder_path,'fits')
+    columns = [
+        "TIME",
+        "SAP_FLUX",
+        "PDCSAP_FLUX",
+        "SAP_FLUX_ERR",
+        "PDCSAP_FLUX_ERR",
+        "CADENCENO",
+    ]
+    filenames_tbl = get_filenames(folder_path, "tbl")
+    filenames_fits = get_filenames(folder_path, "fits")
     for filename in filenames_fits:
-        os.remove(folder_path+filename)
+        os.remove(folder_path + filename)
     for idx, filename in enumerate(filenames_tbl):
-        if(idx>=0):            
-            data = ascii.read(folder_path + filename).to_pandas()
-            data = data[['TIME','SAP_FLUX','PDCSAP_FLUX','SAP_FLUX_ERR','PDCSAP_FLUX_ERR','CADENCENO']].dropna()
-            data = normalize_data(data)
+        data = ascii.read(folder_path + filename).to_pandas()
+        data = data.interpolate(method="linear", columns=columns)
+        data = data[columns].dropna()
+        data = normalize_data(data)
 
-            remove_noise(data, data.PDCSAP_FLUX,'PDC_RAW_MEDIAN')
-            remove_noise(data, data.FPDC,'PDC_NORM_MEDIAN')
-    
-            data.to_csv(folder_path+filename.replace('.tbl','.csv'),index=False)
-            os.remove(folder_path+filename)
+        remove_noise(data, data.PDCSAP_FLUX, "PDC_RAW_FILT")
+        remove_noise(data, data.FPDC, "PDC_NORM_FILT")
 
-def get_filenames(folder_path,extension):
-    return (f for f in os.listdir(folder_path) if f.endswith('.' + extension))
+        data = data.dropna()
+        data.to_csv(folder_path + filename.replace(".tbl", ".csv"), index=False)
+        os.remove(folder_path + filename)
+
+
+def get_filenames(folder_path, extension):
+    return list(f for f in os.listdir(folder_path) if f.endswith("." + extension))
+
 
 def normalize_data(data):
     r = copy.deepcopy(data)
-    for c,ec,c2,ec2 in zip(col,ecol,col2,ecol2):
+    for c, ec, c2, ec2 in zip(col, ecol, col2, ecol2):
         medf = np.median(r[c])
         norm = r[c] / medf - 1
         enorm = r[ec] / medf
@@ -59,123 +72,163 @@ def normalize_data(data):
         r[ec2] = enorm
     return r
 
-def plot_data(data_x, data_y, label_x='Time', label_y='Flux', title=''):
-    plt.figure(1,dpi=300)
+
+def plot_data(data_x, data_y, label_x="Time", label_y="Flux", title=""):
+    plt.figure(1, dpi=300)
     sns.lineplot(data_x, data_y)
     plt.xlabel(label_x)
     plt.ylabel(label_y)
     plt.title(title)
     plt.show()
 
-def remove_noise(df,data,col_name='MEDIAN'):
+
+def remove_noise(df, data, col_name="FILT"):
     """
     inputs:
     df: dataframe containing the data
     data: data to be adjusted
     field_name: name of the column to be added to the dataframe
     """
-    df[col_name] = data.rolling(30).median()
+    df[col_name] = data.rolling(170).mean()
+
 
 def get_signal_parameters(tt, yy):
     '''Fit sin to the input time sequence, and return fitting parameters "amp", "omega", "phase", "offset", "freq", "period" and "fitfunc"'''
     tt = np.array(tt)
     yy = np.array(yy)
-    ff = np.fft.fftfreq(len(tt), (tt[1]-tt[0]))   # assume uniform spacing
+    ff = np.fft.fftfreq(len(tt), (tt[1] - tt[0]))  # assume uniform spacing
     Fyy = abs(np.fft.fft(yy))
-    guess_freq = abs(ff[np.argmax(Fyy[1:])+1])   # excluding the zero frequency "peak", which is related to offset
-    guess_amp = np.std(yy) * 2.**0.5
+    guess_freq = abs(
+        ff[np.argmax(Fyy[1:]) + 1]
+    )  # excluding the zero frequency "peak", which is related to offset
+    guess_amp = np.std(yy) * 2 * 2.0 ** 0.5
     guess_offset = np.mean(yy)
-    guess = np.array([guess_amp, 2.*np.pi*guess_freq, 0., guess_offset])
+    guess = np.array([guess_amp, 2.0 * np.pi * guess_freq, 0.0, guess_offset])
 
-    def sinfunc(t, A, w, p, c):  return A * np.sin(w*t + p) + c
-    popt, pcov = optimize.curve_fit(sinfunc, tt, yy, p0=guess, maxfev=5000)
+    def sinfunc(t, A, w, p, c):
+        return A * np.sin(w * t + p) + c
+
+    popt, pcov = optimize.curve_fit(sinfunc, tt, yy, p0=guess, maxfev=7000)
     A, w, p, c = popt
-    f = w/(2.*np.pi)
-    fitfunc = lambda t: A * np.sin(w*t + p) + c
-    return {"amp": A, "omega": w, "phase": p, "offset": c, "freq": f, "period": 1./f, "fitfunc": fitfunc, "maxcov": np.max(pcov), "rawres": (guess,popt,pcov)}
+    f = w / (2.0 * np.pi)
+    fitfunc = lambda t: A * np.sin(w * t + p) + c
+    return {
+        "amp": A,
+        "omega": w,
+        "phase": p,
+        "offset": c,
+        "freq": f,
+        "period": 1.0 / f,
+        "fitfunc": fitfunc,
+        "maxcov": np.max(pcov),
+        "rawres": (guess, popt, pcov),
+    }
+
 
 def round_elements(list, n_places):
-    return [ round(elem, n_places) for elem in list ]
+    return [round(elem, n_places) for elem in list]
+
 
 def remove_outliers(list):
     mean = np.mean(list, axis=0)
     sd = np.std(list, axis=0)
     new_list = [x for x in list if (x > mean - 2 * sd)]
-    new_list = [x for x in new_list if (x < mean + 2*sd)]
+    new_list = [x for x in new_list if (x < mean + 2 * sd)]
     return new_list
 
+
 def get_period(periods_list):
-    periods = round_elements(periods_list,3)
-    if(len(periods)>1):
+    periods = round_elements(periods_list, 3)
+    if len(periods) > 1:
         periods = remove_outliers(periods)
     try:
         period = mode(periods)
     except:
         # group most_common output by frequency
-        periods_freqs = groupby(Counter(periods).most_common(), lambda x:x[1])
+        periods_freqs = groupby(Counter(periods).most_common(), lambda x: x[1])
         # pick off the first group (highest frequency)
-        periods_freqs = [val for val,count in next(periods_freqs)[1]]
-        if(len(periods_freqs)>1):
+        periods_freqs = [val for val, count in next(periods_freqs)[1]]
+        if len(periods_freqs) > 1:
             periods_freqs = remove_outliers(periods_freqs)
-        if(stdev(periods_freqs)<=5):
-            period = mean(periods_freqs)            
+        if stdev(periods_freqs) <= 5:
+            period = mean(periods_freqs)
         else:
-            try:            
-                periods_2 = round_elements(periods_freqs,2)
-                mode_2 = mode(periods_2)                
-                
-                period = mean(list(x for x in periods if mode_2-1 <= x <= mode_2+1))
+            try:
+                periods_2 = round_elements(periods_freqs, 2)
+                mode_2 = mode(periods_2)
+
+                period = mean(list(x for x in periods if mode_2 - 1 <= x <= mode_2 + 1))
             except:
-                try:                    
-                    periods_1 = round_elements(periods_freqs,1)
-                    mode_1 = mode(periods_1)                
-                    
-                    period = mean(list(x for x in periods if mode_1-1 <= x <= mode_1+1))
+                try:
+                    periods_1 = round_elements(periods_freqs, 1)
+                    mode_1 = mode(periods_1)
+
+                    period = mean(
+                        list(x for x in periods if mode_1 - 1 <= x <= mode_1 + 1)
+                    )
                 except:
                     try:
-                        periods_0 = round_elements(periods_freqs,0)
-                        mode_0 = mode(periods_0)                
-                        
-                        period = mean(list(x for x in periods if mode_0-1 <= x <= mode_0+1))
+                        periods_0 = round_elements(periods_freqs, 0)
+                        mode_0 = mode(periods_0)
+
+                        period = mean(
+                            list(x for x in periods if mode_0 - 1 <= x <= mode_0 + 1)
+                        )
                     except:
-                        periods_freqs = groupby(Counter(periods_0).most_common(), lambda x:x[1])
-                        periods_freqs = [val for val,count in next(periods_freqs)[1]]            
+                        periods_freqs = groupby(
+                            Counter(periods_0).most_common(), lambda x: x[1]
+                        )
+                        periods_freqs = [val for val, count in next(periods_freqs)[1]]
                         period_freq = remove_outliers(periods_freqs)[0]
-                        period = mean(list(x for x in periods if period_freq-1 <= x <= period_freq+1))
+                        period = mean(
+                            list(
+                                x
+                                for x in periods
+                                if period_freq - 1 <= x <= period_freq + 1
+                            )
+                        )
     finally:
-        period = round(period,3)
+        period = round(period, 3)
         return period
+
 
 def remove_single_quotes(path):
     file_in = path
-    file_out = path+'-ed'
-    with open(r""+file_in+"", 'r') as infile, \
-        open(r""+file_out+"", 'w') as outfile:
+    file_out = path + "-ed"
+    with open(r"" + file_in + "", "r") as infile, open(
+        r"" + file_out + "", "w"
+    ) as outfile:
         data = infile.read()
-        data = data.replace("\'", "")
+        data = data.replace("'", "")
         outfile.write(data)
     os.remove(file_in)
-    os.rename(file_out,file_out.replace('-ed',''))
+    os.rename(file_out, file_out.replace("-ed", ""))
+
 
 def get_download_url(page_text):
-    big = page_text[page_text.find('<big>'):page_text.find('</big>')]
-    link = big[big.find('/cgi'):big.find('t"')+1]
+    big = page_text[page_text.find("<big>") : page_text.find("</big>")]
+    link = big[big.find("/cgi") : big.find('t"') + 1]
     return link
 
+
 def download_files(kic):
-    r = requests.post(BASE_URL+"/cgi-bin/IERDownload/nph-IERDownload",
-                data={'id': kic,
-                        'inventory_mode': 'id_single',
-                        'idtype': 'source',
-                        'dataset':'kepler',
-                        'resultmode':'webpage'})
+    r = requests.post(
+        BASE_URL + "/cgi-bin/IERDownload/nph-IERDownload",
+        data={
+            "id": kic,
+            "inventory_mode": "id_single",
+            "idtype": "source",
+            "dataset": "kepler",
+            "resultmode": "webpage",
+        },
+    )
     link = get_download_url(r.text)
-    url = BASE_URL+link
+    url = BASE_URL + link
     response = requests.get(url, stream=True)
     kic_str = str(kic)
-    kic_file_name = str(kic)+".bat"
-    folder_path = BASE_PATH+kic_str+"/"
-    path = BASE_PATH+kic_str+"/"+kic_file_name
+    kic_file_name = str(kic) + ".bat"
+    folder_path = BASE_PATH + kic_str + "/"
+    path = BASE_PATH + kic_str + "/" + kic_file_name
 
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
@@ -187,9 +240,9 @@ def download_files(kic):
     remove_single_quotes(path)
 
     print("downloading kepler files")
-    os.chdir(r""+folder_path+"")
+    os.chdir(r"" + folder_path + "")
     subprocess.call([kic_file_name])
     os.chdir("../../../")
     print("kepler files downloaded")
     return folder_path
-    
+
